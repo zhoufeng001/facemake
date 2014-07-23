18d32dbd4a13324fc4b47d46f86d3716fa1e6317
is_zhoufeng <is_zhoufeng@163.com>
2014-07-24 01:21:38 +0800

    配置mybatis

---
+++ facemake/facemake-dal/src/test/java/com/facemake/dal/test/Test01.java
@@ -0,0 +1,11 @@
+package com.facemake.dal.test;
+
+import org.junit.Test;
+
+public class Test01 {
+	@Test
+	public void test01(){
+		System.out.println("xxx");
+	}
+
+}
---
+++ facemake/facemake-dal/src/test/java/com/facemake/dal/test/Test02.java
@@ -0,0 +1,45 @@
+package com.facemake.dal.test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.facemake.dal.mapper.FmUserMapper;
+import com.facemake.dal.pojo.FmUser;
+import com.facemake.dal.pojo.FmUserCriteria;
+
+public class Test02 {
+
+
+	public static void main(String[] args) {
+		SqlSessionFactory sqlSessionFactory = null; 
+		Reader reader = null;  
+		try {  
+			reader  = Resources.getResourceAsReader("mybatis-config.xml");  
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);  
+		} catch (IOException e) {  
+			e.printStackTrace();  
+		}  
+
+		SqlSession session = sqlSessionFactory.openSession();  
+		try {  
+			FmUserMapper fmUserMapper = session.getMapper(FmUserMapper.class) ;
+
+			FmUserCriteria query = new FmUserCriteria() ;
+			query.createCriteria().andNickEqualTo("is_zhoufeng") ;
+			List<FmUser> users = fmUserMapper.selectByExample(query) ;
+
+			System.out.println(users.size());
+
+			System.out.println(fmUserMapper);
+		} finally {  
+			session.close();  
+		}  
+	}
+
+}
